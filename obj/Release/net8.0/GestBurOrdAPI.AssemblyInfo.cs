9ac2b4c99c3bb5a44970bb0fec160267e532489d
JacemBenAhmed <jacem0987654321@gmail.com>
2025-05-02 14:00:46 +0100

    final api

--- obj/Release/net8.0/GestBurOrdAPI.AssemblyInfo.cs
+++ obj/Release/net8.0/GestBurOrdAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestBurOrdAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04724625fed7593fe2dd3e702d4307099de7251f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestBurOrdAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestBurOrdAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
